ed0b09018f7dff96bb4fe9c16fb3566274ad75be
EmanueleEsposito <manu2022@blu.it>
2022-06-13 11:34:38 +0200

    fine esercizio

--- src/jsnacks/JSnack1.java
+++ src/jsnacks/JSnack1.java
@@ -1,10 +1,32 @@
+//Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+//Esegui questo programma in due versioni, con il for e con il while.
+
 package jsnacks;
+
+import java.util.Scanner;
 
 public class JSnack1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] numeroUtente = new int[5];
+		
+Scanner scan = new Scanner(System.in);
 
+
+
+for(int index = 0; index<numeroUtente.length; index++ ) {
+	System.out.println("Scrivi un numero  ");
+	numeroUtente[index] = scan.nextInt();
+
+}
+int sommaUtente=0;
+for( int i=0; i<numeroUtente.length; i++);{
+	System.out.println(numeroUtente[sommaUtente]);
+}
+		
+scan.close();	
 	}
 
 }
---
+++ src/jsnacks/JSnack2.java
@@ -0,0 +1,26 @@
+//Chiedi all'utente di inserire un numero, se è pari stampa il numero,
+//se è dispari stampa il numero successivo
+package jsnacks;
+
+import java.util.Scanner;
+
+public class JSnack2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan = new Scanner(System.in);
+int numeroUtente;
+System.out.println("Scrivi un numero ");
+numeroUtente=scan.nextInt();
+boolean numeroPari=false;
+if(numeroPari) {
+	System.out.println();
+	
+}
+
+
+
+scan.close();
+	}
+
+}
